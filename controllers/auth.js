3163729cef5c0210b9743e8668f40671404b02f9
PRmars86 <pengruima@gmail.com>
2023-05-21 22:48:39 -0700

    Set up AWS SES

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,16 @@
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
 
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID,
-    region: process.env.AWS_REGION,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
 });
 
 const ses = new AWS.SES({ apiVersion: '2010-12-01' });
 
-
 exports.register = (req, res) => {
-    //console.log("Controller", req.body)
-    const { name, email, password } = req.body
+    // console.log('REGISTER CONTROLLER', req.body);
+    const { name, email, password } = req.body;
     const params = {
         Source: process.env.EMAIL_FROM,
         Destination: {
@@ -21,26 +20,26 @@ exports.register = (req, res) => {
         Message: {
             Body: {
                 Html: {
-                    Charset: 'UTF_8',
-                    Data: `<html><body><h1> Hello ${name}</h1 style="colore:red;"><p>Test email</p></body></html>`
+                    Charset: 'UTF-8',
+                    Data: `<html><body><h1>Hello ${name}</h1 style="color:red;"><p>Test email</p></body></html>`
                 }
             },
             Subject: {
-                Charset: 'UTF_8',
-                Data: "Compelete your registeration"
+                Charset: 'UTF-8',
+                Data: 'Complete your registration'
             }
         }
-    }
+    };
 
     const sendEmailOnRegister = ses.sendEmail(params).promise();
 
     sendEmailOnRegister
         .then(data => {
-            console.log('email submitted to SES', data)
-            res.send("Email sent")
+            console.log('email submitted to SES', data);
+            res.send('Email sent');
         })
         .catch(error => {
-            console.log('ses email on register', error)
-            res.send("Email failed")
-        })
-}
+            console.log('ses email on register', error);
+            res.send('email failed');
+        });
+};
