65eac0fe4f1c3809bf06116bec3a05fda2af0bbf
PRmars86 <pengruima@gmail.com>
2023-05-30 20:54:51 -0700

    create link

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.DATABASE_CLOUD, { useNewUrlParser: true, useUnified
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const categoryRoutes = require('./routes/category');
+const linkRoutes = require('./routes/link');
 
 //app middlewares
 app.use(morgan('dev'));
@@ -26,6 +27,7 @@ app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
+app.use('/api', linkRoutes);
 
 const port = process.env.PORT;
 
