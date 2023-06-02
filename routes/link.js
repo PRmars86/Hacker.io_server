d3f8685c27d44cc1a7b780d83e8a32aba4576e74
PRmars86 <pengruima@gmail.com>
2023-06-01 17:43:01 -0700

    1

--- routes/link.js
+++ routes/link.js
@@ -6,12 +6,12 @@ const { linkCreateValidator, linkUpdateValidator } = require('../validators/link
 const { runValidation } = require('../validators');
 
 // controllers
-const { requireSignin, authMiddleware } = require('../controllers/auth');
+const { requireSignin, authMiddleware, adminMiddleware } = require('../controllers/auth');
 const { create, list, read, update, remove, clickCount } = require('../controllers/link');
 
 // routes
 router.post('/link', linkCreateValidator, runValidation, requireSignin, authMiddleware, create);
-router.get('/links', list);
+router.post('/links', requireSignin, adminMiddleware, list);
 router.put('/click-count', clickCount);
 router.get('/link/:id', read);
 router.put('/link/:id', linkUpdateValidator, runValidation, requireSignin, authMiddleware, update);
