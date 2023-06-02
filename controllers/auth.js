c57e7563ac3877308c4625d6b7bd80fdeb46b123
PRmars86 <pengruima@gmail.com>
2023-06-01 21:36:15 -0700

    admin update/delete link

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const Link = require('../models/link');
 const AWS = require('aws-sdk');
 const jwt = require('jsonwebtoken');
 const { registerEmailParams, forgotPasswordEmailParams } = require('../helpers/email');
@@ -233,4 +234,22 @@ exports.resetPassword = (req, res) => {
             });
         });
     }
+};
+
+exports.canUpdateDeleteLink = (req, res, next) => {
+    const { id } = req.params;
+    Link.findOne({ _id: id }).exec((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Could not find link'
+            });
+        }
+        let authorizedUser = data.postedBy._id.toString() === req.user._id.toString();
+        if (!authorizedUser) {
+            return res.status(400).json({
+                error: 'You are not authorized'
+            });
+        }
+        next();
+    });
 };
